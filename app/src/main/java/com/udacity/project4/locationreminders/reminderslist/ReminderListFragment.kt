fd3e6e214c72302c38a03422b9d3fb3c7db803a8
dengweidavid <wei.deng@machineswithvision.com>
2023-05-23 22:20:26 +0100

    Finish User Authentication part

--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -1,9 +1,13 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
@@ -65,7 +69,11 @@ class ReminderListFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.logout -> {
-                // TODO: add the logout implementation
+                AuthUI.getInstance().signOut(requireContext())
+                val activity = requireActivity()
+                val intent = Intent(requireContext(), AuthenticationActivity::class.java)
+                activity.startActivity(intent)
+                activity.finish()
             }
         }
         return super.onOptionsItemSelected(item)
