023e2aa49704fbc3707e062b2349d69a9f649963
dengweidavid <wei.deng@machineswithvision.com>
2023-05-27 20:39:01 +0100

    RemindersListViewModelTest is done

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -33,7 +33,7 @@ class RemindersListViewModelTest {
     }
 
     @Test
-    fun loadReminders_givenErrorResult_showsSnackBarErrorMessage() =
+    fun loadReminders_givenErrorDataSource_showsSnackBarErrorMessageAndShowNoData() =
         runTest {
             // GIVEN: the dataSource return errors.
             fakeDataSource.setShouldReturnError(true)
@@ -45,6 +45,53 @@ class RemindersListViewModelTest {
             // THEN
             // Show error message in SnackBar
             Assert.assertEquals(errorMessage, viewModel.showSnackBar.getOrAwaitValue())
+            // showNoData is true
+            Assert.assertEquals(true, viewModel.showNoData.getOrAwaitValue())
+        }
+
+    @Test
+    fun loadReminders_givenValidDataSource_showsReminderList() =
+        runTest {
+            // GIVEN items
+            val reminder1 = ReminderDataItem(
+                "Title todo1",
+                "Description todo1",
+                "Location todo1",
+                250.0,
+                350.0)
+            fakeDataSource.saveReminder(reminder1.toReminderDTO())
+
+            val reminder2 = ReminderDataItem(
+                "Title todo2",
+                "Description todo2",
+                "Location todo2",
+                -250.0,
+                350.0)
+            fakeDataSource.saveReminder(reminder2.toReminderDTO())
+
+            // WHEN load reminders
+            viewModel.loadReminders()
+
+            // THEN
+            // data is the same with source
+            Assert.assertEquals(listOf(reminder1,reminder2),viewModel.remindersList.getOrAwaitValue())
+            // showNoData is false
+            Assert.assertEquals(false, viewModel.showNoData.getOrAwaitValue())
+        }
+
+    @Test
+    fun loadReminders_emptyDataSource_noReminders() =
+        runTest {
+            // GIVEN no items
+            fakeDataSource.deleteAllReminders()
+
+            // WHEN load reminders
+            viewModel.loadReminders()
+
+            // THEN
+            // Size is zero
+            val loadedItems = viewModel.remindersList.getOrAwaitValue()
+            Assert.assertEquals(0, loadedItems.size)
             // showNoData is true
             Assert.assertEquals(true, viewModel.showNoData.getOrAwaitValue())
         }
